50c75b3acba73b9fd0857dc80ed4e5903a778413
banzaben892 <banzaben892@gmail.com>
2026-06-02 11:02:31 +0200

    Update MyApp.kt

--- app/src/main/java/com/ludomasterpro/MyApp.kt
+++ app/src/main/java/com/ludomasterpro/MyApp.kt
@@ -1,10 +1,14 @@
 package com.ludomasterpro
 
 import android.app.Application
+import android.util.Log
 
 class MyApp : Application() {
+
     override fun onCreate() {
         super.onCreate()
+
+        Log.d("MyApp", "CrashHandler installé")
 
         Thread.setDefaultUncaughtExceptionHandler(
             CrashHandler(this)
